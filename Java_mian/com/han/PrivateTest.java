0b618e61f86ca05847f56bbd8d66f41b2c4f0bf2
Han shenquan <1173956412@qq.com>
2015-12-09 19:09:01 +0800

    modify1

--- Java_mian/com/han/PrivateTest.java
+++ Java_mian/com/han/PrivateTest.java
@@ -1,7 +1,17 @@
 package com.han;
-
+import static java.lang.System.out;
 /**
  * Created by HSQ on 2015/12/9.
  */
 public class PrivateTest {
+    private int i= 5;
+    private int fun(){
+        return i;
+    }
+    public static void main(String []args){
+        PrivateTest t= new PrivateTest();
+        out.println(t.i);
+        out.println(t.fun());
+
+    }
 }
